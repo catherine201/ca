bf23e8c9b75b66dd1662ddc6cd6c1012eee34d81
zero9527 <zero8297183@163.com>
2019-05-07 09:58:11 +0800

    修改 no-unused-vars报错

--- src/views/transactionCoinEdit/index.js
+++ src/views/transactionCoinEdit/index.js
@@ -1,12 +1,5 @@
 import React, { Component } from 'react';
-import {
-  Input,
-  InputNumber,
-  Button,
-  message,
-  DatePicker,
-  LocaleProvider
-} from 'antd';
+import { Input, Button, message, DatePicker, LocaleProvider } from 'antd';
 import zh_CN from 'antd/lib/locale-provider/zh_CN';
 import moment from 'moment';
 import 'moment/locale/zh-cn';
